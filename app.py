8c52fb370be826b5cf57e24610800a02574af409
akhwatfun <akhwatfun1004@gmail.com>
2023-08-31 12:41:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,8 +149,9 @@ def run():
     if st.sidebar.button('Predict!'):
         df = input_df
         st.write(df)
-        loaded_model = pickle.load(open('best_model_logreg.pkl', 'rb'))
-        prediction = loaded_model.predict(df)       
+        with open("best_model_logreg.pkl", 'rb') as file:  
+            loaded_model = pickle.load(file)
+        prediction = loaded_model.predict(df)  
         result = ['Cannot Adaptive' if prediction == 0 else 'Yes Can Adaptive']
         st.subheader('Prediction: ')
         output = str(result[0])
